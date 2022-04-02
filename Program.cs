01ac02f776f04e78b377b473b5d7ea069dfd391d
chackee <pavelkralik24@gmail.com>
2022-04-02 14:12:17 +0200

    -Saving changes from last lecture

--- Program.cs
+++ Program.cs
@@ -11,22 +11,14 @@ using System.Text.RegularExpressions;
 
 namespace ExampleProj
 { 
-    class Person
-    {
-        public string name;
-        public int age;
-    }
-
     public class Program
     {
-
         public static void Main(string[] args)
         {
-            List<Person> people = new List<Person>() { new Person() { name = "John", age = 25 } };
-            List<Person> people2 = new List<Person>() { new Person() { name = "John", age = 25 } };
-
-            Debug.WriteLine(people.SequenceEqual(people2));
+            int i = 3;
+            int j = 5;
             Debug.WriteLine("Hello World!");
+            Debug.WriteLine(i+j);
         }
     }
 }
