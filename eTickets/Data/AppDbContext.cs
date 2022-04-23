57a5357c663a3c3d40f017c6241e18790e4a63c5
chackee <pavelkralik24@gmail.com>
2022-04-23 21:06:42 +0200

    Entity Framework Core Tools NuGet Package installed. Startup.cs file deleted Program.cs file returned from backup as it was in the beginning
    SQL connection string set in AppDbContext
    
    State before first migration

--- eTickets/Data/AppDbContext.cs
+++ eTickets/Data/AppDbContext.cs
@@ -12,9 +12,17 @@ namespace eTickets.Data
         public DbSet<Producer> Producers { get; set; }
 
         //Constructor
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        public AppDbContext(DbContextOptions<AppDbContext> options) 
+            : base(options)
         {
+        }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=KRAL-PC\\SQLEXPRESS;Initial Catalog=ecommerce-app-db;Trusted_Connection=True;TrustServerCertificate=True;Integrated Security=True;");
+            }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -1,4 +1,27 @@
-using eTickets;
+var builder = WebApplication.CreateBuilder(args);
 
-var app = Startup.InitializeApp(args);
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
 app.Run();
