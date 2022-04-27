61413df18d4e72421ec620a02aed14b8683fb4ee
chackee <pavelkralik24@gmail.com>
2022-04-27 15:44:21 +0200

    -Created IActorsService interface where all methods are defined for Actors. -Created ActorsService where the code for methods is written. Also ctor of this class gets AppDbContext for db communication
    -Added async and await tasks.
    -Added service configuration into program.cs and its called by Actors Controller to get data from db.

--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -1,14 +1,19 @@
 using eTickets.Data;
+using eTickets.Data.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// DbContext configuration
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString"));
 });
+
+//Services configuration
+builder.Services.AddScoped<IActorsService, ActorsService>();
 
 var app = builder.Build();
 
