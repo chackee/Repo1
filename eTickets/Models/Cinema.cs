d93d983df36b5389fab2f4527e29ade14db5113d
chackee <pavelkralik24@gmail.com>
2022-05-20 00:17:48 +0200

    Create Cinema function done

--- eTickets/Models/Cinema.cs
+++ eTickets/Models/Cinema.cs
@@ -7,11 +7,17 @@ namespace eTickets.Models
     {
         [Key]
         public int Id { get; set; }
+
         [Display(Name = "Cinema Logo")]
+        [Required(ErrorMessage = "Cinema logo is required")]
         public string Logo { get; set; }
+
         [Display(Name = "Name")]
+        [Required(ErrorMessage = "Cinema Name is required")]
         public string Name { get; set; }
+
         [Display(Name = "Description")]
+        [Required(ErrorMessage = "Cinema description is required")]
         public string Description { get; set; }
 
         //Relationships
