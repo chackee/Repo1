bc92513f2905ce804aaf56fac1485bbd553c1e8e
chackee <pavelkralik24@gmail.com>
2022-05-19 22:08:47 +0200

    Configuration of Cinemas service

--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -15,6 +15,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 //Services configuration
 builder.Services.AddScoped<IActorsService, ActorsService>();
 builder.Services.AddScoped<IProducersService, ProducersService>();
+builder.Services.AddScoped<ICinemasService, CinemasService>();
 
 var app = builder.Build();
 
