8c02e9ceee4b036b94d27db074409535fd5fcaa8
chackee <pavelkralik24@gmail.com>
2022-05-22 15:34:53 +0200

    Added Create Function with saving to database.

--- eTickets/Data/Services/IMoviesService.cs
+++ eTickets/Data/Services/IMoviesService.cs
@@ -8,5 +8,6 @@ namespace eTickets.Data.Services
     {
         Task<Movie> GetMovieByIdAsync(int id);
         Task<NewMovieDropdownsVM> GetNewMovieDropdownsValues();
+        Task AddNewMovieAsync(NewMovieVM data);
     }
 }
