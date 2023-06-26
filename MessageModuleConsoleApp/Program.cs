96bbe564bcc0481fce4bc298201fdadc416367b2
chackee <pavelkralik24@gmail.com>
2023-06-26 23:52:18 +0200

    Created console app that implements SMS module

---
+++ MessageModuleConsoleApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using MessageModule.SMS;
+
+namespace MessageModuleConsoleApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string message = GenerateMessage();
+                if (string.IsNullOrWhiteSpace(message)) { Console.WriteLine("Application will be closed"); Console.ReadKey(); return; }
+
+                var sms = new SMSModule();
+
+                sms.Process(message);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("An error occurred during runtime and message could not be sent: {0}", ex.Message.ToString());
+                Console.ReadKey();
+            }
+            
+        }
+
+        private static string GenerateMessage()
+        {
+            string result = "";
+
+            while(string.IsNullOrWhiteSpace(result))
+            {
+                Console.WriteLine("Please enter a message body or enter \'0\' to cancel: ");
+
+                result = Console.ReadLine().Trim();
+                if (result == "0") return "";
+            }
+
+            return result;
+        }
+    }
+}
---
+++ MessageModuleConsoleAppv2/Program.cs
@@ -0,0 +1,64 @@
+﻿using MessageModule.SMS;
+
+namespace MessageModuleConsoleAppv2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                bool isInitialized = InitializeApp();
+
+                if (!isInitialized) { Console.WriteLine("Application could not be initialized"); Console.ReadKey(); return; }
+
+                string message = GenerateMessage();
+                if (string.IsNullOrWhiteSpace(message)) { Console.WriteLine("Application will be closed"); Console.ReadKey(); return; }
+
+                var sms = new SMSModule();
+
+                sms.Process(message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred during runtime and message could not be sent: {0}", ex.Message.ToString());
+                Console.ReadKey();
+            }
+
+        }
+
+        private static string GenerateMessage()
+        {
+            string result = "";
+
+            while (string.IsNullOrWhiteSpace(result))
+            {
+                Console.WriteLine("Please enter a message body or enter \'0\' to cancel: ");
+
+                result = Console.ReadLine().Trim();
+                if (result == "0") return "";
+            }
+
+            return result;
+        }
+
+        private static bool InitializeApp()
+        {
+            try
+            {
+                string sid = "PleaseEnterSID";
+                string token = "PleaseEnterToken";
+                string source = "SourcePhoneNumber";
+                string[] targets = new string[1] { "TargetPhoneNumbers" };
+
+                SMSModuleInstaller.Instance.Configure(sid, token, source, targets);
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
